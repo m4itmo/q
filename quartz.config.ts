bbe3886ecafdacd6b6239c51ae4b2ad8e421dcbe
std46d6b <dev@m5k.ru>
2024-05-28 00:02:58 +0300

    Quartz sync: May 28, 2024, 12:02 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "itmo.m5k.ru",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
